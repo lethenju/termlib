e10ad2979174d63aae719b9685f3f26cf55306e0
Julien LE THENO <julien.letheno@gmail.com>
2019-02-18 17:38:50 +0100

    delete old position

--- src/cursor.h
+++
@@ -1,8 +0,0 @@
-#ifndef CURSOR_H
-#define CURSOR_H
-#include "termlib_types.h"
-
-void cursor_init(termlib_context *ctx, int posX, int posY, char rep);
-
-void display_cursor(termlib_screen *screen, termlib_cursor *cursor);
-#endif
