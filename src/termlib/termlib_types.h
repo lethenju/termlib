4feb57bff4c26ee4bd42f08e4f17960f6b82b1c1
Julien LE THENO <julien.letheno@gmail.com>
2019-02-22 15:55:40 +0100

    doc on termlib_types

--- src/termlib/termlib_types.h
+++ src/termlib/termlib_types.h
@@ -1,34 +1,47 @@
+/**
+ * termlib version 0.2 (c) Julien LE THENO
+ * (c) MIT LICENSE
+ */
 #ifndef TERMLIB_TYPES_H
 #define TERMLIB_TYPES_H
 #include <pthread.h>
 
+/** represents a simple 'pixel' : one display unit of the screen
+ */
 typedef struct {
-    char rep;
-} pixel;
+    char rep; // representation of the pixel
+} pixel; // TODO add color fg/bg as pixel fields
 
+/** represents the screen : the terminal area used by termlib
+ */
 typedef struct {
-    unsigned int width;
-    unsigned int height;
-    pthread_t display_thread;
-    pixel*  pixels;
+    unsigned int width;       // width of the 'screen' in columns
+    unsigned int height;      // height of the 'screen' in rows
+    pthread_t display_thread; // identifier of the tread that draws the screen
+    pixel*  pixels;           // pointer to the pixel array
 } termlib_screen;
 
+/** input structure, that links to input-related data
+ */ 
 typedef struct {
-    pthread_t input_thread;
-} termlib_input;
+    pthread_t input_thread; // identifier of the thread that manages input
+} termlib_input; // TODO Add a input stack to better manage the input data
 
+/** represents the cursor object. 
+ */
 typedef struct {
-    int posX;
-    int posY;
-    char rep;
-} termlib_cursor;
+    int posX; // X position of the cursor
+    int posY; // Y position of the cursor
+    char rep; // character representation of the cursor
+} termlib_cursor; // TODO Should be moved to winman instead
 
+/** whole termlib context pointer that gather all data about the current app, termlib-wise
+ */
 typedef struct {
-    termlib_screen* screen;
-    termlib_input* input;
-    termlib_cursor cursor;
-    volatile int exit;
+    termlib_screen* screen; // pointer to the screen structure
+    termlib_input* input;   // pointer to the input structure
+    termlib_cursor cursor;  // pointer to the cursor structure
+    volatile int exit;      // exit flag. When it is turned to 1, the threads will exit and the application will stop.
 } termlib_context;
-
 
 #endif
